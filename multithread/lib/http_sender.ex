42132f8c23601dfd4c0c3433cbb94e29960af225
Philibert Dugas <philibert.dugas@shopify.com>
2016-08-01 22:54:51 -0400

    Detect the end of the test

--- multithread/lib/http_sender.ex
+++ multithread/lib/http_sender.ex
@@ -3,12 +3,8 @@ require Logger
 defmodule HTTPSender do
   def send_and_log_request(method, url, assert) do
     atom_method = String.downcase(method) |> String.to_atom
-    Task.start(
-      fn ->
-        {time, %HTTPotion.Response{status_code: status_code}} = :timer.tc(fn -> HTTPotion.request(atom_method, url) end)
-        log_request(url, status_code, assert, time)
-      end
-    )
+    {time, %HTTPotion.Response{status_code: status_code}} = :timer.tc(fn -> HTTPotion.request(atom_method, url) end)
+    log_request(url, status_code, assert, time)
   end
 
   defp log_request(url, status_code, assert, time) do
--- multithread/lib/runner.ex
+++ multithread/lib/runner.ex
@@ -1,7 +1,7 @@
 defmodule Runner do
   def main do
     struct = get_struct
-    launch_thread_groups(struct.elt)
+    launch_thread_groups(struct.elt, [])
   end
 
   defp get_struct do
@@ -9,29 +9,31 @@ defmodule Runner do
     Poison.decode!(content, as: %Elt{})
   end
 
-  defp launch_thread_groups([head | tail]) do
-    Task.start(fn -> launch_threads(MapHelper.to_struct(ThreadGroup, head)) end)
-    launch_thread_groups(tail)
+  defp launch_thread_groups([head | tail], tasks) do
+    task = Task.async(fn -> launch_threads(MapHelper.to_struct(ThreadGroup, head), []) end)
+    launch_thread_groups(tail, List.insert_at(tasks, 0, task))
   end
 
-  defp launch_thread_groups([]) do
+  defp launch_thread_groups([], tasks) do
+    Task.yield_many(tasks, 30000)
     IO.puts "Thread groups finished"
   end
 
-  defp launch_threads(group = %ThreadGroup{}) do
+  defp launch_threads(group = %ThreadGroup{}, tasks) do
     case group.threads do
-      0 -> IO.puts "Finished starting threads"
+      0 ->
+        Task.yield_many(tasks, 30000)
+        IO.puts "Finished threads"
       _ ->
-        Task.start fn ->
-          launch_request(MapHelper.to_struct(Request, group.requests), group.repetitions)
-        end
-        launch_threads(%{ group | threads: group.threads - 1 })
+        task = Task.async(fn -> launch_request(MapHelper.to_struct(Request, group.requests), group.repetitions) end)
+        launch_threads(%{ group | threads: group.threads - 1 }, List.insert_at(tasks, 0, task))
     end
   end
 
   defp launch_request(request = %Request{}, repetition) do
     case repetition do
-      0 -> IO.puts "finished sending request"
+      0 ->
+        IO.puts "finished sending request"
       _ ->
         HTTPSender.send_and_log_request(request.method, request.url, request.assert)
         launch_request(request, repetition - 1)
